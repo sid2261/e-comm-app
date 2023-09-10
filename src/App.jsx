15186f49dca76d9793601e2274b99b024a74c333
sid2261 <siddhartha2251@gmail.com>
2023-09-10 09:01:14 +0530

    created route

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import './App.css';
+import Button from "@mui/material/Button";
+import { createBrowserRouter } from "react-router-dom";
+import { createRoutesFromElements } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<h1>This is index route</h1>}></Route>
+
+  )
+)
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+
+export default App
