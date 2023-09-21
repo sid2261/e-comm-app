5475d1903c38efef5cfe071ef10612955b088d75
sid2261 <siddhartha2251@gmail.com>
2023-09-21 23:52:02 +0530

    partialy implemented search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,29 @@ import { Typography } from "@mui/material";
 import ShoppingCartSharpIcon from "@mui/icons-material/ShoppingCartSharp";
 import { useSelector } from 'react-redux';
 import { getItemCount } from '../utils';
+import { styled, alpha } from '@mui/material/styles';
+
+const Search = styled("section")(({theme})=>({
+  position:"relative",
+  borderRadius: theme.shape.borderRadius,
+  display: "flex",
+  backgroundColor: alpha
+}))
+function SearchBar(){
+  return <h1>this is the search bar</h1>
+}
 
 
 function Header() {
   const cartItems = useSelector((state)=>state.cart?.value);
-  const count = getItemCount(cartItems)
+  const count = getItemCount(cartItems);
   return (
     <AppBar position="sticky">
         <Toolbar>
-            <Typography 
-                variant='h6'
-                color="inherit"
-                sx={{flexGrow: 1}}
-            >
+            <Typography variant='h6'color="inherit">
                 GrowMart
             </Typography>
+            <SearchBar/>
             <Box sx={{display: {xs:"none", md:"flex"}}}>
             <IconButton size="large" aria-label="shows cart items count" color="inherit">
               <Badge badgeContent={count} color = "error">
